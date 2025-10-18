f65714cdf046a41cda67bc0d2828052cc4589b2c
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-10-18 19:04:10 +0200

    changes

--- osu-tracker/header/config.h
+++ osu-tracker/header/config.h
@@ -4,72 +4,42 @@
 #include "console.h"
 #include <crow/mustache.h>
 
-#define FOREACH_DATA_KEY(F) \
-	F(level) \
-	F(rankedScore) \
-	F(totalScore) \
-	F(ppRank) \
-	F(pp) \
-	F(ppv1) \
-	F(acc) \
-	F(playtime) \
-	F(playcount) \
-	F(totalHits) \
-	F(silverSS) \
-	F(goldSS) \
-	F(silverS) \
-	F(goldS) \
-	F(a) \
-	F(b) \
-	F(c) \
-	F(d) \
-	F(totalSS) \
-	F(totalS) \
-	F(clears) \
-	F(totalClears) \
-	F(completion) \
-	F(scoreRank) \
-	F(targetRank) \
-	F(targetUser) \
-	F(targetScore)
-
-constexpr bool str_eq(const char* a, const char* b) {
-	while (*a && *b) {
-		if (*a != *b) return false;
-		++a; ++b;
-	}
-	return *a == *b;
-}
-
-#define GEN_CASE(name, idx) if (str_eq(key, #name)) return idx;
-
 namespace config {
-	enum class gameMode {
+	enum gameMode {
 		osu = 0
 		,taiko = 1
 		,fruits = 2
 		,mania = 3
 	};
 
-	enum class server {
+	enum server {
 		bancho = 0
 		,titanic = 1
 	};
 
 	struct application {
-		// definition and initialization
-		#define applicationFields(_field) \
-			_field(int, osuId, 0) \
-			_field(int, clientId, 0) \
-			_field(std::string, clientSecret, "") \
-			_field(int, apiInterval, 7000) \
-			_field(config::gameMode, gameMode, config::gameMode::osu) \
-			_field(config::server, server, config::server::bancho)
+		int osuId = 0;
+		int clientId = 0;
+		std::string clientSecret;
+		int apiInterval = 7000;
+		config::gameMode gameMode = config::gameMode::fruits;
+		config::server server = config::server::titanic;
+
+		std::vector<std::tuple<std::string, std::string>> toArray() const {
+			return {
+				{"osuId", std::to_string(osuId)}
+				,{ "clientId", std::to_string(clientId)}
+				,{ "clientSecret", clientSecret}
+				,{ "apiInterval", std::to_string(apiInterval)}
+				,{ "gameMode", std::to_string(gameMode)}
+				,{ "server", std::to_string(server)}
+			};
+		}
 	};
 
 	struct user {
-		std::string username = "";
-		std::string avatar = "";
+		std::string username;
+		std::string avatar;
 
 		std::vector<std::tuple<std::string, std::string>> toArray() const {
 			return {
@@ -95,12 +65,6 @@ namespace config {
 		,f_percent = 5
 	};
 
-	enum dataSettings : uint32_t {
-		POSITIVE = 1 << 0
-		,DISPLAY = 1 << 1
-		,SINGLE = 1 << 2
-	};
-
 	enum serverSettings : uint32_t {
 		BANCHO = 1 << 0
 		,TITANIC = 1 << 1
@@ -115,7 +79,9 @@ namespace config {
 		std::string change;
 		dataType dataType;
 		formatType formatType;
-		u_int32_t dataSettings;
+		bool single;
+		bool positive;
+		bool display;
 		u_int32_t serverSettings;
 
 		std::vector<std::tuple<std::string, std::string>> toArray() const {
@@ -133,47 +99,72 @@ namespace config {
 			};
 		}
 	};
-// positive display single
+
 	namespace data {
-		static inline std::vector<dataEntry> arr {
-			{"level",		"Level",			1, "", "",	"", dataType::d_float,		formatType::f_decimal,	POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"rankedScore","Ranked Score",		2, "", "",	"", dataType::d_longLong,	formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"totalScore",	"Total Score",		3, "", "",	"", dataType::d_longLong,	formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"ppRank",		"PP Rank",			4, "", "",	"", dataType::d_int,		formatType::f_rank,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"pp",			"PP",				5, "", "",	"", dataType::d_float,		formatType::f_decimal,	POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"ppv1",		"PPv1",				6, "", "",	"", dataType::d_float,		formatType::f_decimal,	POSITIVE | DISPLAY,	TITANIC}
-			,{"acc",		"Accuracy",			7, "", "",	"", dataType::d_float,		formatType::f_percent,	POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"playtime",	"Play Time",		8, "", "",	"", dataType::d_longLong,	formatType::f_time,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"playcount",	"Play Count",		9, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"totalHits",	"Total Hits",		10, "", "",	"", dataType::d_longLong,	formatType::f_int,		POSITIVE | DISPLAY,	TITANIC}
-
-			,{"silverSS",	"Rank SSH",			11, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"goldSS",		"Rank SS",			12, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"silverS",	"Rank SH",			13, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"goldS",		"Rank S",			14, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"a",			"Rank A",			15, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"b",			"Rank B",			16, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"c",			"Rank C",			17, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"d",			"Rank D",			18, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"totalSS",	"Total SS",			19, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"totalS",		"Total S",			20, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-
-			,{"clears",		"Profile Clears",	21, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"totalClears","Total Clears",		22, "", "",	"", dataType::d_int,		formatType::f_int,		POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"completion",	"Completion%",		23, "", "",	"", dataType::d_float,		formatType::f_percent,	POSITIVE | DISPLAY,	BANCHO | TITANIC}
-			,{"scoreRank",	"Score Rank",		24, "", "",	"", dataType::d_int,		formatType::f_rank,		POSITIVE | DISPLAY,	BANCHO}
-
-			,{"targetRank",	"Target Rank",		25, "", "",	"", dataType::d_int,		formatType::f_rank,		DISPLAY | SINGLE,	BANCHO}
-			,{"targetUser",	"Target Player",	26, "", "",	"", dataType::d_string,		formatType::f_string,	DISPLAY | SINGLE,	BANCHO}
-			,{"targetScore","Target Score",		27, "", "",	"", dataType::d_longLong,	formatType::f_int,		DISPLAY | SINGLE,	BANCHO}
+		std::vector<dataEntry> arr {
+			{"level",		"Level",			1, "", "",	"", dataType::d_float,		formatType::f_decimal,	false, true, true,	BANCHO | TITANIC}
+			,{"rankedScore","Ranked Score",		2, "", "",	"", dataType::d_longLong,	formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"totalScore",	"Total Score",		3, "", "",	"", dataType::d_longLong,	formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"ppRank",		"PP Rank",			4, "", "",	"", dataType::d_int,		formatType::f_rank,		false, true, true,	BANCHO | TITANIC}
+			,{"pp",			"PP",				5, "", "",	"", dataType::d_float,		formatType::f_decimal,	false, true, true,	BANCHO | TITANIC}
+			,{"ppv1",		"PPv1",				6, "", "",	"", dataType::d_float,		formatType::f_decimal,	false, true, true,	TITANIC}
+			,{"acc",		"Accuracy",			7, "", "",	"", dataType::d_float,		formatType::f_percent,	false, true, true,	BANCHO | TITANIC}
+			,{"playtime",	"Play Time",		8, "", "",	"", dataType::d_longLong,	formatType::f_time,		false, true, true,	BANCHO | TITANIC}
+			,{"playcount",	"Play Count",		9, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"totalHits",	"Total Hits",		10, "", "",	"", dataType::d_longLong,	formatType::f_int,		false, true, true,	TITANIC}
+			,{"silverSS",	"Rank SSH",			11, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"goldSS",		"Rank SS",			12, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"silverS",	"Rank SH",			13, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"goldS",		"Rank S",			14, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"a",			"Rank A",			15, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"b",			"Rank B",			16, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"c",			"Rank C",			17, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"d",			"Rank D",			18, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"totalSS",	"Total SS",			19, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"totalS",		"Total S",			20, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"clears",		"Profile Clears",	21, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"totalClears","Total Clears",		22, "", "",	"", dataType::d_int,		formatType::f_int,		false, true, true,	BANCHO | TITANIC}
+			,{"completion",	"Completion%",		23, "", "",	"", dataType::d_float,		formatType::f_percent,	false, true, true,	BANCHO | TITANIC}
+			,{"scoreRank",	"Score Rank",		24, "", "",	"", dataType::d_int,		formatType::f_rank,		false, true, true,	BANCHO}
+			,{"targetRank",	"Target Rank",		25, "", "",	"", dataType::d_int,		formatType::f_rank,		true,  true, true,	BANCHO}
+			,{"targetUser",	"Target Player",	26, "", "",	"", dataType::d_string,		formatType::f_string,	true,  true, true,	BANCHO}
+			,{"targetScore","Target Score",		27, "", "",	"", dataType::d_longLong,	formatType::f_int,		true,  true, true,	BANCHO}
 		};
 
 		static inline std::vector<dataEntry> arrFormatted;
 
-		static constexpr int getIndex(const char* key) {
-		#define INDEX_CASE(name) GEN_CASE(name, __COUNTER__)
-			FOREACH_DATA_KEY(INDEX_CASE)
-				return -1;
+		enum key {
+			level = 0,
+			rankedScore = 1,
+			totalScore = 2,
+			ppRank = 3,
+			pp = 4,
+			ppv1 = 5,
+			acc = 6,
+			playtime = 7,
+			playcount = 8,
+			totalHits = 9,
+			silverSS = 10,
+			goldSS = 11,
+			silverS = 12,
+			goldS = 13,
+			a = 14,
+			b = 15,
+			c = 16,
+			d = 17,
+			totalSS = 18,
+			totalS = 19,
+			clears = 20,
+			totalClears = 21,
+			completion = 22,
+			scoreRank = 23,
+			targetRank = 24,
+			targetUser = 25,
+			targetScore = 26
+		};
+
+		inline constexpr int getIndex(key arrKey) {
+			return static_cast<int>(arrKey);
 		}
 	};
 
@@ -182,30 +173,16 @@ namespace config {
 			std::string content;
 			if (std::ifstream file{ dir_entry.path() }; file.is_open()) {
 				for (std::string line; std::getline(file, line); ) {
-					for (dataEntry row : config::data::arr) {
+					for (size_t i = 0; i < config::data::arr.size(); ++i) {
 						// raw
-						line = ext::replace(line, "{{" + row.key + "_init_raw}}", row.init);
-						line = ext::replace(line, "{{" + row.key + "_change_raw}}", row.change);
-						line = ext::replace(line, "{{" + row.key + "_current_raw}}", row.current);
+						line = ext::replace(line, "{{" + config::data::arr[i].key + "_init_raw}}", config::data::arr[i].init);
+						line = ext::replace(line, "{{" + config::data::arr[i].key + "_change_raw}}", config::data::arr[i].change);
+						line = ext::replace(line, "{{" + config::data::arr[i].key + "_current_raw}}", config::data::arr[i].current);
 
 						// formatted
-						line = ext::replace(
-							line,
-							"{{" + row.key + "_init}}",
-							config::data::arrFormatted[config::data::getIndex(row.key.c_str())].init
-						);
-						
-			            line = ext::replace(
-				            line,
-				            "{{" + row.key + "_change}}",
-				            config::data::arrFormatted[config::data::getIndex(row.key.c_str())].change
-			            );
-						
-			            line = ext::replace(
-							line,
-							"{{" + row.key + "_current}}",
-							config::data::arrFormatted[config::data::getIndex(row.key.c_str())].current
-			            );
+						line = ext::replace(line, "{{" + config::data::arr[i].key + "_init}}", config::data::arrFormatted[i].init);
+			            line = ext::replace(line, "{{" + config::data::arr[i].key + "_change}}", config::data::arrFormatted[i].change);
+			            line = ext::replace(line, "{{" + config::data::arr[i].key + "_current}}", config::data::arrFormatted[i].current);
 					}
 					content += line + "\n";
 				}
@@ -221,17 +198,20 @@ namespace config {
 		}
 	}
 
-	static void writeConfig() {
+	void writeConfig() {
 		// ordered_json bc it sorts alphabetically by default
 		nlohmann::ordered_json config;
-		for (const auto& [key, value] : config::application::instance().toArray()) {
-			if (value == "") {
+		config::application application;
+		for (const auto& [key, value] : application.toArray()) {
+			if (std::empty(value)) {
 				config["Main"][key] = nullptr;
 				continue;
 			}
 			config["Main"][key] = value;
 		}
 		for (const config::dataEntry _vecData : config::data::arr) {
+			bool display = (_vecData.dataSettings & DISPLAY) != 0;
+
 			config["Display"][_vecData.key]["visible"] = _vecData.display;
 			config["Display"][_vecData.key]["sort"] = _vecData.sort;
 		}
@@ -241,7 +221,7 @@ namespace config {
 		file.close();
 	}
 
-	static void readConfig() {
+	void readConfig() {
 		if (std::ifstream file{ "config.json" }; file.is_open()) {
 			nlohmann::ordered_json config = nlohmann::ordered_json::parse(file);
 			for (auto& main : config["Main"].items()) {
@@ -267,11 +247,11 @@ namespace config {
 		}
 	}
 	
-	static void rmConfig() {
+	void rmConfig() {
 		std::filesystem::remove("config.json");
 	}
 
-	static void createTemplateExample() {
+	void createTemplateExample() {
 		std::string content;
 		for (size_t i = 0; i < config::data::arr.size(); i++) {
 			content += config::data::arr[i].key + " (raw):\n";
